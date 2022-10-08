8e26ef5c00cea2d76b311e5b57e28958af23ba98
Tharun1828 <91731118+Tharun1828@users.noreply.github.com>
2022-10-08 18:29:27 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/SadKidBGMZ/Rocky_Umar75.git /Rocky_Umar75
+  git clone https://github.com/SadKidBGMZ/bgm_auto_filter_bot.git /bgm_auto_filter_bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone https://github.com/SadKidBGMZ/Rocky_Umar75
+  git clone $UPSTREAM_REPO /bgm_auto_filter_bot
 fi
-ls
-cd SadKidBGMZ/Rocky_Umar75
-ls
-$PWD
+cd /bgm_auto_filter_bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
